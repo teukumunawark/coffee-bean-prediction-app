87dd00094bfb477b62d7fb3d11579c7827bfbb2c
teukumunawark <munawartmk@gmail.com>
2023-03-06 23:06:47 +0700

    adding button widget

---
+++ lib/presentation/widgets/button_widget.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:predict_coffee/utils/constants.dart';
+
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget({
+    super.key,
+    required this.title,
+    required this.onPressed,
+    this.borderRadius = 50,
+    this.width = double.maxFinite,
+    this.height = 55,
+  });
+
+  final String title;
+  final double width;
+  final double height;
+  final double borderRadius;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      width: width,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor: kPrimary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+        child: Text(
+          title,
+          style: kHeading6.copyWith(
+            color: kBackground,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
