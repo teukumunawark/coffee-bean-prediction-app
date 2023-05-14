cc9cf1384524668f76663a74b54f754aadc3ea89
teukumunawark <munawartmk@gmail.com>
2023-05-14 23:35:04 +0700

    update

--- lib/injection.dart
+++ lib/injection.dart
@@ -2,16 +2,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
-import 'package:predict_coffee/data/api/prediction_api.dart';
-import 'package:predict_coffee/data/repositories/data_repository.dart';
-import 'package:predict_coffee/data/services/firebase_services.dart';
-import 'package:predict_coffee/domain/repositories/domain_repository.dart';
-import 'package:predict_coffee/domain/use_case/predict_use_case/create_multi_predict.dart';
-import 'package:predict_coffee/domain/use_case/predict_use_case/create_single_predict.dart';
-import 'package:predict_coffee/domain/use_case/user_use_case/get_current_user.dart';
-import 'package:predict_coffee/presentation/states_bloc/auth/auth_bloc.dart';
-import 'package:predict_coffee/presentation/states_bloc/multi_predict/multi_predict_bloc.dart';
-import 'package:predict_coffee/presentation/states_bloc/single_predict/single_predict_bloc.dart';
+import 'data/api/prediction_api.dart';
+import 'data/repositories/data_repository.dart';
+import 'data/services/firebase_services.dart';
+import 'domain/repositories/domain_repository.dart';
+import 'domain/use_case/predict_use_case/create_multi_predict.dart';
+import 'domain/use_case/predict_use_case/create_single_predict.dart';
+import 'domain/use_case/user_use_case/get_current_user.dart';
+import 'presentation/states_bloc/auth/auth_bloc.dart';
+import 'presentation/states_bloc/multi_predict/multi_predict_bloc.dart';
+import 'presentation/states_bloc/single_predict/single_predict_bloc.dart';
 import 'domain/use_case/user_use_case/create_history.dart';
 import 'domain/use_case/user_use_case/user_login.dart';
 import 'domain/use_case/user_use_case/user_logout.dart';
@@ -25,12 +25,7 @@ Future<void> init() async {
   locator.registerFactory(() => MultiPredictBloc(locator()));
   locator.registerFactory(() => HistoryBloc(locator()));
   locator.registerFactory(
-    () => AuthBloc(
-      locator(),
-      locator(),
-      locator(),
-      locator(),
-    ),
+    () => AuthBloc(locator(), locator(), locator(), locator()),
   );
 
   // USE CASE INJECTION
@@ -45,15 +40,12 @@ Future<void> init() async {
   // REPOSITORY INJECTION
   locator.registerLazySingleton<DomainRepository>(
     () => DataRepositoryIMPL(
-      dataSource: locator(),
-      firebaseDataSource: locator(),
-    ),
+        dataSource: locator(), firebaseDataSource: locator()),
   );
 
   // API INJECTION
   locator.registerLazySingleton<ApiDataSource>(
-    () => CoffeeDataSourceIMPL(dio: locator()),
-  );
+      () => CoffeeDataSourceIMPL(dio: locator()));
 
   // FIREBASE INJECTION
   locator.registerLazySingleton<FirebaseDataSource>(
