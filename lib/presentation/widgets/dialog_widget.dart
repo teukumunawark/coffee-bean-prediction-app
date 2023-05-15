852c5143567c77bfa92b0cfd42ddf8633249b54f
teukumunawark <munawartmk@gmail.com>
2023-05-15 22:48:06 +0700

    dialog widget update

--- lib/presentation/widgets/dialog_widget.dart
+++ lib/presentation/widgets/dialog_widget.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:lottie/lottie.dart';
 import 'package:ndialog/ndialog.dart';
+import '../routes/app_route.dart';
 
 import '../../utils/constants.dart';
 import 'button_widget.dart';
@@ -47,8 +48,57 @@ NDialog prosesDialog(BuildContext context, imageBloc) {
           borderRadius: 6,
           height: 45,
           onPressed: () {
-            Navigator.pop(context);
+            router.pop();
             imageBloc.pickImage(ImageSource.gallery);
+          },
+        ),
+      ),
+    ],
+  );
+}
+
+NDialog multiImageDialog(BuildContext context, multiImageBloc) {
+  return NDialog(
+    dialogStyle: DialogStyle(
+      titleDivider: true,
+      contentTextStyle: GoogleFonts.poppins(),
+    ),
+    title: Center(
+      child: Text(
+        "Kosong".toUpperCase(),
+        style: kHeading3,
+      ),
+    ),
+    content: SizedBox(
+      height: 210,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 150,
+            child: LottieBuilder.asset(
+              'assets/lottie/neurallink.json',
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            "Pilih gambar terlebih dahulu untuk melakukan proses gambar",
+            style: kBodyText,
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    ),
+    actions: [
+      Padding(
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+        child: ButtonWidget(
+          title: "Pilih Gambar",
+          borderRadius: 6,
+          height: 45,
+          onPressed: () {
+            router.pop();
+            multiImageBloc.pickImages(ImageSource.gallery);
           },
         ),
       ),
